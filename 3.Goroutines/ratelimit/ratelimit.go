10f0a43fa27516798a63c3ea55b6bb7ef55f3d4f
BetterCallUgL <drushockek@gmail.com>
2024-02-09 14:29:45 +0300

    i've made ratelimit(hard)

--- 3.Goroutines/ratelimit/ratelimit.go
+++ 3.Goroutines/ratelimit/ratelimit.go
@@ -8,6 +8,11 @@ import (
 
 // Limiter is precise rate limiter with context support.
 type Limiter struct {
+	cnt      chan int
+	d        time.Duration
+	stopped  chan bool
+	maxCount int
+	timer    *time.Timer
 }
 
 var ErrStopped = errors.New("limiter stopped")
@@ -15,13 +20,40 @@ var ErrStopped = errors.New("limiter stopped")
 // NewLimiter returns limiter that throttles rate of successful Acquire() calls
 // to maxSize events at any given interval.
 func NewLimiter(maxCount int, interval time.Duration) *Limiter {
-	panic("not implemented")
+	ch1 := make(chan int, maxCount)
+	ch1 <- 0
+	ch2 := make(chan bool, 1)
+	ch2 <- false
+	var timer *time.Timer = time.NewTimer(interval)
+	return &Limiter{ch1, interval, ch2, maxCount, timer}
 }
 
 func (l *Limiter) Acquire(ctx context.Context) error {
-	panic("not implemented")
+	flag := <-l.stopped
+	l.stopped <- flag
+	if flag {
+		return ErrStopped
+	}
+	select {
+	case cur := <-l.cnt:
+		if cur == 0 {
+			l.timer.Reset(l.d)
+		}
+		cur++
+		if cur != l.maxCount {
+			l.cnt <- cur
+		}
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-l.timer.C:
+		l.cnt = make(chan int, 1)
+		l.cnt <- 0
+	}
+	return nil
 }
 
 func (l *Limiter) Stop() {
-	panic("not implemented")
+	<-l.stopped
+	l.timer.Stop()
+	l.stopped <- true
 }
