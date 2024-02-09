92877ef606ed907270a661963003f7665de96b52
BetterCallUgL <drushockek@gmail.com>
2024-02-09 14:33:06 +0300

    fixed ratelimit

--- 3.Goroutines/ratelimit/ratelimit.go
+++ 3.Goroutines/ratelimit/ratelimit.go
@@ -20,7 +20,7 @@ var ErrStopped = errors.New("limiter stopped")
 // NewLimiter returns limiter that throttles rate of successful Acquire() calls
 // to maxSize events at any given interval.
 func NewLimiter(maxCount int, interval time.Duration) *Limiter {
-	ch1 := make(chan int, maxCount)
+	ch1 := make(chan int, 1)
 	ch1 <- 0
 	ch2 := make(chan bool, 1)
 	ch2 <- false
