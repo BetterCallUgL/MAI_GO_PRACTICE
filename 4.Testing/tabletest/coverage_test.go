b79128b9e400378e7c6207f2d4cff2bb62fa89b0
BetterCallUgl <drushockek@gmail.com>
2024-03-07 20:47:06 +0300

    done tabletest

--- 4.Testing/tabletest/coverage_test.go
+++ 4.Testing/tabletest/coverage_test.go
@@ -11,18 +11,27 @@ func TestParseDuration(t *testing.T) {
 	var tests = []struct {
 		in  string
 		out time.Duration
-		err error
 	}{
-		{"+1s", time.Second, nil},
-		{"aboba",time.Second,nil},
+		{"+1s", time.Second},
+		{"-1.2s", -1200 * time.Millisecond},
+		{"0", 0},
+		{"a", 0},
+		{"", 0},
+		{".", 0},
+		{"10mp", 0},
+		{"0.000000000000000000000000329999999999999999999999999999999999999999999999999", 0},
+		{"999999999999999999999999999.2", 0},
+		{".123ms", 123 * time.Microsecond},
+		{"-1.2s.", 0},
+		{"1234567899999h", 0},
+		{"9223372036854775808", 0},
+		{"0.9223372036854775808", 0},
 	}
 
 	for _, tt := range tests {
-		time, err := ParseDuration(tt.in)
+		time, _ := ParseDuration(tt.in)
 		if time != tt.out {
-			t.Fatalf("TimeError: expected %s,got %s\n", tt.out.String(), time.String())
-		} else if err != tt.err {
-			t.Fatalf("ErrorProblem: expected %v,got %v\n", tt.err.Error(), err)
+			t.Fatalf("TimeError: expected \"%s\",got \"%s\"\n", tt.out.String(), time.String())
 		}
 	}
 }
