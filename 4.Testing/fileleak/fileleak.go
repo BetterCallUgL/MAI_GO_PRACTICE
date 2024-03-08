216c065dadd0fb9b21aa42b5102318a021853e58
BetterCallUgL <drushockek@gmail.com>
2024-03-08 21:26:22 +0300

    finally done fileleak

--- 4.Testing/fileleak/fileleak.go
+++ 4.Testing/fileleak/fileleak.go
@@ -1,10 +1,45 @@
 package fileleak
 
+import (
+	"io/fs"
+	"os"
+)
+
 type testingT interface {
 	Errorf(msg string, args ...interface{})
 	Cleanup(func())
 }
 
+func FillMap(dict map[string]int, dir []fs.DirEntry) {
+	for _, file := range dir {
+		desc, _ := os.Readlink("/proc/self/fd/" + file.Name())
+		if _, ok := dict[desc]; !ok {
+			dict[desc] = 0
+		}
+		dict[desc]++
+	}
+}
+
 func VerifyNone(t testingT) {
-	panic("implement me")
+	path := "/proc/self/fd"
+
+	dir, _ := os.ReadDir(path)
+	old := make(map[string]int)
+	FillMap(old, dir)
+
+	t.Cleanup(func() {
+		dir, _ = os.ReadDir(path)
+		new := make(map[string]int)
+		FillMap(new, dir)
+
+		for key, newVal := range new {
+			if oldVal, ok := old[key]; ok {
+				if newVal > oldVal {
+					t.Errorf("Detected unclosed file clone: %v", key)
+				}
+			} else {
+				t.Errorf("Detected unclosed file:%v", key)
+			}
+		}
+	})
 }
